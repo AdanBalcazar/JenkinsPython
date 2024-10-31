3cfdfa8840aec350a1fb7cb9fdfc2fc0502d8766
AdanBalcazar <47190187+AdanBalcazar@users.noreply.github.com>
2024-10-31 12:36:06 -0600

    Add files via upload

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -4,7 +4,7 @@ from Experiment import Greeter
 class MyTestCase(unittest.TestCase):
     def test_default_greeting_set(self):
         greeter = Greeter()
-        self.assertEqual(greeter.message, 'Hello world!')
+        self.assertEqual(greeter.message, 'Heeeello world!')
 
 if __name__ == '__main__':
     unittest.main()
